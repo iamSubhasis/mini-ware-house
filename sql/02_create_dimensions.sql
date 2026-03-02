0dd36f4f56bc0e67a5431f3d463ccdfb9877d7ff
iamSubhasis <redmahapatra579@gmail.com>
2026-03-02 11:03:26 +0530

    Fix gitignore and update dimension script

--- sql/02_create_dimensions.sql
+++ sql/02_create_dimensions.sql
@@ -12,7 +12,7 @@ supplier_name text not null unique
 );
 
 create table if not exists dw.dim_product(
-product key serial primary key,
+product_key serial primary key,
 item_code text not null unique,
 item_description text,
 item_type text not null
